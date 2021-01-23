444b84d7f43876d3b17235fa0fdb56800b61a52c
Eva Wong <suddenlyeva@gmail.com>
2021-01-22 22:43:52 -0800

    move actionbar stuff into coroutines

--- GameEngine/CoreEngine/CoreEngine/game/scenes/battlefield.lua
+++ GameEngine/CoreEngine/CoreEngine/game/scenes/battlefield.lua
@@ -1,17 +1,34 @@
 local env = require("./game/environment")
 local scene = env.NewScene()
 
-local actionBar = scene.CreateSprite("UIActionBar")
-actionBar.AnchorPoint = DeviceVector(0,0,0,0)
-actionBar.Position = DeviceVector(0,20,0,100)
+local character = {
+  SPD = 100,
+  ATB = 0
+}
 
-local atb = 0
-wait()
-while true do
-  local delta = wait()
-  atb = atb + 100
-  if atb >= 10000 then
-    atb = 0
+local character2 = {
+  SPD = 150,
+  ATB = 0
+}
+
+DisplayActionBar = function(character, offset)
+
+  local actionBar = scene.CreateSprite("UIActionBar")
+  actionBar.AnchorPoint = DeviceVector(0,0,0,0)
+  actionBar.Position = DeviceVector(0,20,0,offset)
+
+  while true do
+    character.ATB = character.ATB + character.SPD
+    if character.ATB >= 10000 then
+      character.ATB = character.ATB - 10000
+    end
+    actionBar.Size = DeviceVector(0,character.ATB/10000 * 300,0,20)
+    wait()
   end
-  actionBar.Size = DeviceVector(0,atb/10000 * 300,0,20)
+
 end
+
+wait()
+
+local t1 = coroutine.wrap(DisplayActionBar)(character, 100)
+local t2 = coroutine.wrap(DisplayActionBar)(character2, 200)
